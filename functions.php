8fd90fd68cf15a210dd43c86b4cc91d040d6deb7
BenSibley <ben@competethemes.com>
2016-09-07 08:32:34 -0400

    get_bloginfo for charset meta element wasn't set

--- functions.php
+++ functions.php
@@ -505,7 +505,7 @@ function ct_tribes_add_meta_elements() {
 
 	$meta_elements = '';
 
-	$meta_elements .= sprintf( '<meta charset="%s" />' . "\n", get_bloginfo( 'charset' ) );
+	$meta_elements .= sprintf( '<meta charset="%s" />' . "\n", esc_html( get_bloginfo( 'charset' ) ) );
 	$meta_elements .= '<meta name="viewport" content="width=device-width, initial-scale=1" />' . "\n";
 
 	$theme    = wp_get_theme( get_template() );

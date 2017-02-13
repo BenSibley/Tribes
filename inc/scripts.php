baba7a8d06cbfe196a3713b83e63be4a3a538d20
BenSibley <ben@competethemes.com>
2017-02-13 11:15:08 -0500

    switched site_url to get_home_url

--- inc/scripts.php
+++ inc/scripts.php
@@ -53,7 +53,7 @@ add_action( 'customize_controls_enqueue_scripts', 'ct_tribes_enqueue_customizer_
 function ct_tribes_enqueue_customizer_post_message_scripts() {
 	wp_enqueue_script( 'ct-tribes-customizer-post-message-js', get_template_directory_uri() . '/js/build/postMessage.min.js', array( 'jquery' ), '', true );
 	wp_localize_script( 'ct-tribes-customizer-post-message-js', 'ct_tribes_postmessage', array(
-		'siteURL'       => esc_url( get_site_url() )
+		'siteURL'       => esc_url( get_home_url() )
 	) );
 
 }

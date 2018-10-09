8021bdd544ec2e304fec0392dcb63eeb7fb65758
BenSibley <ben@competethemes.com>
2018-10-09 13:48:05 -0400

    updated FontAwesome to 5.3.1

--- inc/scripts.php
+++ inc/scripts.php
@@ -18,7 +18,7 @@ function ct_tribes_load_scripts_styles() {
 		'closeChildMenu' => esc_html_x( 'close dropdown menu', 'close the dropdown menu', 'tribes' )
 	) );
 
-	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.min.css' );
+	wp_enqueue_style( 'ct-tribes-font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/all.min.css' );
 
 	wp_enqueue_style( 'ct-tribes-style', get_stylesheet_uri() );
 

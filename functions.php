bfbce469238ec40893bda740c7e25b58b9edc044
BenSibley <ben@competethemes.com>
2018-11-13 10:21:22 -0500

    Gutenberg editor styles

--- functions.php
+++ functions.php
@@ -67,6 +67,8 @@ if ( ! function_exists( ( 'ct_tribes_theme_setup' ) ) ) {
 		// Gutenberg - wide & full images
 		add_theme_support( 'align-wide' );
 		add_theme_support( 'align-full' );
+		// Gutenberg - load editor stylesheet
+		add_theme_support('editor-styles');
 
 		load_theme_textdomain( 'tribes', get_template_directory() . '/languages' );
 	}

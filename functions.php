d4e09da0ff48ba6a063af5ff674c49e8e53cacbf
BenSibley <ben@competethemes.com>
2018-12-04 13:57:15 -0500

    added Goodreads to social icons

--- functions.php
+++ functions.php
@@ -340,6 +340,7 @@ if ( ! function_exists( 'ct_tribes_social_array' ) ) {
 			'flickr'        => 'tribes_flickr_profile',
 			'foursquare'    => 'tribes_foursquare_profile',
 			'github'        => 'tribes_github_profile',
+			'goodreads'   	=> 'tribes_goodreads_profile',
 			'google-plus'   => 'tribes_googleplus_profile',
 			'google-wallet' => 'tribes_google_wallet_profile',
 			'hacker-news'   => 'tribes_hacker-news_profile',
